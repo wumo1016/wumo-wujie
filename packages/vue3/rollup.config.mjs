a7669d7b3f7fde61e0be5584d6684c280a1ed181
wumo1016 <760478279@qq.com>
2024-01-22 22:39:35 +0800

    feat: vue3组件

---
+++ packages/vue3/rollup.config.mjs
@@ -0,0 +1,22 @@
+import { terser } from 'rollup-plugin-terser'
+import typescript2 from 'rollup-plugin-typescript2'
+// import nodeResolve from '@rollup/plugin-node-resolve'
+import path from 'path'
+
+const resolve = p => path.resolve(__dirname, p)
+
+export default {
+  input: './index.ts',
+  output: [
+    {
+      file: './dist/index.js',
+      format: 'es'
+    }
+  ],
+  external: ['vue'],
+  plugins: [
+    typescript2() // 解析ts
+    // nodeResolve(), // 解析第三方模块
+    // terser() // 压缩代码
+  ]
+}
